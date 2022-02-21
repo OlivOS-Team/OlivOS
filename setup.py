3ebba18d183653bfd69270f4a0028e99a330199f
仑质 <benzenpenxil@foxmail.com>
2022-02-22 00:48:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as f:
   long_description = f.read()
 
 setuptools.setup(name='olivos',
-    version='0.0.4',
+    version='0.0.5',
     description='OlivOS - Witness Union',
     long_description=long_description,
     long_description_content_type='text/markdown',
