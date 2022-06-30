6d4a6a854adee2374fc05114368be6d99ebc145b
Amber-Keter <1761089294@qq.com>
2022-06-30 11:55:04 +0800

    新增输入流构造方法以实现输入流

--- OlivOS/API.py
+++ OlivOS/API.py
@@ -14,6 +14,10 @@ _  / / /_  /  __  / __ | / /_  / / /____ \
 @Desc      :   None
 '''
 
+import time
+import uuid
+import re
+import os
 import sys
 import json
 import multiprocessing
@@ -1190,6 +1194,142 @@ class Event(object):
         else:
             res_data = self.__get_version_info(flag_log = True)
         return res_data
+    
+    #此处以下定义输入流方法
+    def input_construct(self,input_list:'list|tuple',plugin_name,default_mode:int = 2,default_max_time:int = 30,default_func:'function|None' = None):
+        mode = default_mode
+        string:str = self.data.message
+        user_id:str = str(self.data.user_id)
+        platform = self.platform['platform']
+        group = None
+        if 'host_id' in self.data.__dict__:            
+            group = str(self.data.host_id)
+        if 'group_id' in self.data.__dict__ :
+            group = str(self.data.group_id)
+        data_path = 'plugin/tmp/Input/{}_input_log.json'.format(plugin_name)
+        if not os.path.exists("plugin/tmp/Input"):  #检验数据文件与目录是否存在
+            os.mkdir("plugin/tmp/Input")
+        if not os.path.exists("plugin/tmp/Input/input_data"):
+            os.mkdir("plugin/tmp/Input/input_data")
+        if not os.path.exists(data_path):   
+            init_data = {}
+            with open(data_path,"w",encoding="utf-8") as file:
+                json.dump(init_data, file ,indent=4 ,ensure_ascii=False)     
+        with open(data_path,"r",encoding='utf8') as f:      #导入输入流缓存数据
+            input_log:dict = json.load(f)
+        if input_log.get(platform,None) == None:    #检查文件结构
+            input_log[platform] = {}
+        if input_log[platform].get('group',None) == None:
+            input_log[platform]['group'] = {}
+        if input_log[platform].get('private',None) == None:
+            input_log[platform]['private'] = {}
+        if input_log[platform]['private'].get(user_id,None) == None:
+            input_log[platform]['private'][user_id] = {}
+        if input_log[platform]['group'].get(group,None) == None:
+            input_log[platform]['group'][group] = {}
+        if input_log[platform]['group'][group].get(user_id,None) == None:
+            input_log[platform]['group'][group][user_id] = {}
+        if input_log[platform]['group'][group].get('all',None) == None:
+            input_log[platform]['group'][group]['all'] = {}
+        input_log_data = input_log.get(platform,{})
+        #获取输入流缓存数据
+        if mode == 1 and group:         #群，接收群内所有人的信息
+            input_log_data = input_log_data.get('group',{}).get(group,{}).get("all",{})
+        elif mode == 2:                 #User，接收指定用户的信息，可理解为全局接收
+            if group:
+                input_log_data = input_log_data.get('group',{}).get(group,{})
+            else:
+                input_log_data = input_log_data.get('private',{})
+            input_log_data = input_log_data.get(user_id,None)
+        elif mode == 3 and group:       #群内User，接收群内指定用户信息，可理解为群聊接收
+            input_log_data = input_log_data.get('group',{}).get(group,{}).get(user_id,{})
+        else:
+            return
+        if input_log_data == {}:      #如果不存在缓存记录，则执行首条输入流指令
+            input_index = 0
+        else:                         #如果存在，则执行相应索引对应的输入流指令
+            input_index = input_log_data.get('index',0)       
+        #执行输入流语句
+        if input_index < len(input_list):
+            input_order:dict = input_list[input_index]
+            re_construct = input_order.get('re','')
+            if re_construct != '':   #如果可作为正则表达式，则对传入数据进行匹配，否则，直接进入函数
+                re_construct = re.compile(re_construct).match(string)
+                if re_construct:
+                    if re_construct.end() == len(string):
+                        re_string = re_construct.groups()
+                    else:
+                        return
+                else:
+                    return
+            else:
+                re_string = None
+            input_func = input_order.get('function',default_func)
+            max_time = input_order.get('max_time',default_max_time)
+            last_time = input_log_data.get('time',time.time())
+            dealt_time = time.time() - last_time
+            cache_file_name = input_log_data.get('cache_file_name',str(uuid.uuid4()))
+            cache_path = 'plugin/tmp/Input/input_data/{}.json'.format(cache_file_name)
+            if dealt_time > max_time:    #检验是否超时
+                return
+            if not os.path.exists(cache_path):   
+                init_data = {}
+                with open(cache_path,"w",encoding="utf-8") as file:
+                    json.dump(init_data, file,indent=4,ensure_ascii=False)     
+            with open(cache_path,"r",encoding='utf8') as f:      #导入输入流变量缓存数据
+                input_cache_data:dict = json.load(f)
+            if input_cache_data.get('input_history_data',None) == None:
+                input_cache_data['input_history_data'] = []
+            #将数据传入回调函数运算
+            cache_data,new_index = input_func(plugin_event=self,string=string,re_string=re_string,cache_data=input_cache_data)
+            this_input_data = {         #构建此次输入流数据
+                'id':user_id,
+                'group':group,
+                'platform':platform,
+                'string':string,
+                're_string':re_string,
+                'time':time.time()
+            }                           #加入历史输入流数据序列
+            cache_data['input_history_data'].append(this_input_data)
+            if type(new_index) is not int:
+                new_index = input_index + 1
+            if 0 <= new_index < len(input_list):    #新序列合法，不结束输入流，缓存数据
+                    input_log_data['time'] = time.time()
+                    input_log_data['index'] = new_index
+                    input_log_data['cache_file_name'] = cache_file_name
+                    with open(data_path, "w", encoding='utf8') as f:
+                        json.dump(input_log, f, indent=4, ensure_ascii=False)
+                    with open(cache_path, "w", encoding='utf8') as f:
+                        json.dump(cache_data, f, indent=4, ensure_ascii=False)
+                    return
+            #新序列不合法，结束输入流，删除缓存数据
+            if mode == 1 and group:         #群，接收群内所有人的信息
+                input_log[platform]['group'][group]['all'] = {}
+            elif mode == 2:                 #User，接收指定用户的信息，可理解为全局接收
+                if group:
+                    input_log[platform]['group'][group][user_id] = {}
+                else:
+                    input_log[platform]['private'][user_id] = {}
+            elif mode == 3 and group:       #群内User，接收群内指定用户信息，可理解为群聊接收
+                input_log[platform]['group'][group][user_id] = {}
+            with open(data_path, "w", encoding='utf8') as f:
+                json.dump(input_log, f, indent=4, ensure_ascii=False)
+            os.remove(cache_path)
+            return
+        else:       #终止输入流并删除缓存
+            if mode == 1 and group:         #群，接收群内所有人的信息
+                input_log[platform]['group'][group]['all'] = {}
+            elif mode == 2:                 #User，接收指定用户的信息，可理解为全局接收
+                if group:
+                    input_log[platform]['group'][group][user_id] = {}
+                else:
+                    input_log[platform]['private'][user_id] = {}
+            elif mode == 3 and group:       #群内User，接收群内指定用户信息，可理解为群聊接收
+                input_log[platform]['group'][group][user_id] = {}
+            with open(data_path, "w", encoding='utf8') as f:
+                json.dump(input_log, f, indent=4, ensure_ascii=False)
+            os.remove(cache_path)
+            return
 
 class Proc_templet(object):
     def __init__(self, Proc_name = 'native_plugin', Proc_type = 'default', scan_interval = 0.001, dead_interval = 1, rx_queue = None, tx_queue = None, control_queue = None, logger_proc = None):
@@ -1296,3 +1436,13 @@ def checkDictByListAnd(checked_obj, check_list):
                 flag_res = False
                 return flag_res
     return flag_res
+
+def Check_re(re_text: str):
+    '''
+    检查传入字符串是不是符合正则规范的字符串
+    '''
+    try:
+        re.compile(re_text)
+        return True
+    except:
+        return False
