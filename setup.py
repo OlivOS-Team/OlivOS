fbe12d50a4c4fa3a8d43f54807eaa8b10a841fa1
仑质 <benzenpenxil@foxmail.com>
2022-08-17 12:14:42 +0800

    pypi版本推进至0.10.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.md', 'r', encoding = 'utf-8') as f:
   long_description = f.read()
 
 setuptools.setup(name='olivos',
-    version='0.10.1',
+    version='0.10.2',
     description='OlivOS - Witness Union',
     long_description=long_description,
     long_description_content_type='text/markdown',
