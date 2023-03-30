46183f721975dec2ab12a632bf98a63d2c3b8a97
仑质 <benzenpenxil@foxmail.com>
2023-03-30 14:34:51 +0800

    新增data/images路径

--- OlivOS/pluginAPI.py
+++ OlivOS/pluginAPI.py
@@ -122,6 +122,8 @@ class shallow(OlivOS.API.Proc_templet):
         releaseDir('./lib')
         releaseDir('./lib/Lib')
         releaseDir('./lib/DLLs')
+        releaseDir('./data')
+        releaseDir('./data/images')
         threading.Thread(target=self.__init_GUI).start()
         # self.set_check_update()
         time.sleep(1)  # 此处延迟用于在终端第一次启动时等待终端初始化，避免日志丢失，后续需要用异步(控制包流程)方案替代
