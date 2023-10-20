b80f4f81c7bec44e8e20c098d9dd24297ef13632
仑质 <benzenpenxil@foxmail.com>
2023-10-20 10:48:19 +0800

    版本推进至0.11.18

--- OlivOS/infoAPI.py
+++ OlivOS/infoAPI.py
@@ -17,8 +17,8 @@ _  / / /_  /  __  / __ | / /_  / / /____ \
 import OlivOS
 
 
-OlivOS_Version = '0.11.17'
-OlivOS_SVN = 137
+OlivOS_Version = '0.11.18'
+OlivOS_SVN = 138
 
 # Compatible    <= Plugin[compatible_svn]                 : Compatible
 # OldCompatible <= Plugin[compatible_svn] < Compatible    : OldCompatible Warn
