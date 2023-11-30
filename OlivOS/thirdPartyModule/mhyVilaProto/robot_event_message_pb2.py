ce70ecb767b049ad1c30d840ca4efd84b63d7524
仑质 <benzenpenxil@foxmail.com>
2023-11-30 17:31:28 +0800

    Update robot_event_message_pb2.py

--- OlivOS/thirdPartyModule/mhyVilaProto/robot_event_message_pb2.py
+++ OlivOS/thirdPartyModule/mhyVilaProto/robot_event_message_pb2.py
@@ -11,7 +11,7 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
-import model_pb2 as model__pb2
+from . import model_pb2 as model__pb2
 
 
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19robot_event_message.proto\x12\x08vila_bot\x1a\x0bmodel.proto\"8\n\x11RobotEventMessage\x12#\n\x05\x65vent\x18\x01 \x01(\x0b\x32\x14.vila_bot.RobotEventB6Z4gopkg.mihoyo.com/vila-bot-go/proto/vila_bot;vila_botb\x06proto3')
