82e1092aa334235ff9553d281156fdc3bc0a334a
willkyu <87601487+willkyu@users.noreply.github.com>
2024-03-01 10:34:37 +0800

    Update OPQBotSDK.py

--- OlivOS/adapter/OPQBot/OPQBotSDK.py
+++ OlivOS/adapter/OPQBot/OPQBotSDK.py
@@ -23,8 +23,7 @@ import base64
 import requests as req
 from urllib import parse
 import traceback
-import os
-import struct
+from .GetImageSize import get_image_size
 
 import OlivOS
 
@@ -551,7 +550,7 @@ class event_action(object):
                     )
                     flag_now_type = 'image'
                     try:
-                        width, height=get_image_size(data_this.data['file'])
+                        width, height, _ = get_image_size(data_this.data['file'])
                     except:
                         width, height=1920, 1080
                 if size_data == count_data\
@@ -782,65 +781,3 @@ def waitForRes(echo:str):
             break
     return res
 
-class UnknownImageFormat(Exception):
-    pass
-
-def get_image_size(file_path):
-    """
-    Return (width, height) for a given img file content - no external
-    dependencies except the os and struct modules from core
-    """
-    size = os.path.getsize(file_path)
-
-    with open(file_path, 'rb') as input:
-        height = -1
-        width = -1
-        data = input.read(30)
-
-        if (size >= 10) and data[:6] in ('GIF87a', 'GIF89a'):
-            # GIFs
-            w, h = struct.unpack("<HH", data[6:10])
-            width = int(w)
-            height = int(h)
-        elif ((size >= 24) and data.startswith(b'\211PNG\r\n\032\n')
-            and (data[12:16] == 'IHDR')):
-            # PNGs
-            w, h = struct.unpack(">LL", data[16:24])
-            width = int(w)
-            height = int(h)
-        elif (size >= 16) and data.startswith(b'\211PNG\r\n\032\n'):
-            # older PNGs?
-            w, h = struct.unpack(">LL", data[8:16])
-            width = int(w)
-            height = int(h)
-        elif (size >= 2) and data.startswith(b'\377\330'):
-            # JPEG
-            msg = " raised while trying to decode as JPEG."
-            input.seek(0)
-            input.read(2)
-            b = input.read(1)
-            try:
-                while (b and ord(b) != 0xDA):
-                    while (ord(b) != 0xFF): b = input.read(1)
-                    while (ord(b) == 0xFF): b = input.read(1)
-                    if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
-                        input.read(3)
-                        h, w = struct.unpack(">HH", input.read(4))
-                        break
-                    else:
-                        input.read(int(struct.unpack(">H", input.read(2))[0])-2)
-                    b = input.read(1)
-                width = int(w)
-                height = int(h)
-            except struct.error:
-                raise UnknownImageFormat("StructError" + msg)
-            except ValueError:
-                raise UnknownImageFormat("ValueError" + msg)
-            except Exception as e:
-                raise UnknownImageFormat(e.__class__.__name__ + msg)
-        else:
-            raise UnknownImageFormat(
-                "Sorry, don't know how to get information from this file."
-            )
-
-    return width, height
