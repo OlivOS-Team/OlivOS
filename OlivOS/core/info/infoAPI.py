a840707ddb8cb6870a219a8845318b3acb63565d
元理 <metaleo@foxmail.com>
2024-06-12 13:08:06 +0800

    0.11.38，更新内置NapCat版本
    Co-Authored-By: 仑质 <benzenpenxil@foxmail.com>

--- OlivOS/core/info/infoAPI.py
+++ OlivOS/core/info/infoAPI.py
@@ -17,8 +17,8 @@ _  / / /_  /  __  / __ | / /_  / / /____ \
 import OlivOS
 
 
-OlivOS_Version = '0.11.37'
-OlivOS_SVN = 157
+OlivOS_Version = '0.11.38'
+OlivOS_SVN = 158
 
 # Compatible    <= Plugin[compatible_svn]                 : Compatible
 # OldCompatible <= Plugin[compatible_svn] < Compatible    : OldCompatible Warn
