3747716faf2cf898454245ef5a5d13364b6bd2af
元理 <metaleo@foxmail.com>
2024-08-03 16:09:16 +0800

    0.11.44，准备发布

--- OlivOS/core/info/infoAPI.py
+++ OlivOS/core/info/infoAPI.py
@@ -17,8 +17,8 @@ _  / / /_  /  __  / __ | / /_  / / /____ \
 import OlivOS
 
 
-OlivOS_Version = '0.11.43'
-OlivOS_SVN = 163
+OlivOS_Version = '0.11.44'
+OlivOS_SVN = 164
 OlivOS_Version_Slogan = '大暑条'
 
 # Compatible    <= Plugin[compatible_svn]                 : Compatible
