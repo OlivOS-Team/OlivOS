a0789a1ea77b747fbcf280d79910f7280a9d58b9
仑质 <benzenpenxil@foxmail.com>
2026-01-15 09:19:54 +0800

    整理代码 hook

--- hook/hook-sqlite3.py
+++ hook/hook-sqlite3.py
@@ -1,9 +1,10 @@
 from PyInstaller.utils.hooks import collect_all  
 
+
 def hook(hook_api):
     packages = ['sqlite3']
     for package in packages:
-        datas, binaries, hiddenimports = collect_all(package)  
-        #hook_api.add_datas(datas)
-        #hook_api.add_binaries(binaries)
+        datas, binaries, hiddenimports = collect_all(package)
+        # hook_api.add_datas(datas)
+        # hook_api.add_binaries(binaries)
         hook_api.add_imports(*hiddenimports)
