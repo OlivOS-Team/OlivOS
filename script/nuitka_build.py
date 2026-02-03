e6ff0dcd6ebcb889011977e782d2662c5cb4b866
仑质 <benzenpenxil@foxmail.com>
2026-02-04 03:11:07 +0800

    重构 nuitka 编译构建的实现方式

---
+++ script/nuitka_build.py
@@ -0,0 +1,73 @@
+import subprocess
+import sys
+import site
+from pathlib import Path
+
+excludeList = [
+    'pip',
+    'setuptools',
+    'wheel',
+    'nuitka',
+    'pyinstaller'
+]
+
+
+def get_all_site_packages():
+    packages = []
+
+    for site_dir in site.getsitepackages():
+        site_path = Path(site_dir)
+        if site_path.exists():
+            for item in site_path.iterdir():
+                if item.name in ['__pycache__', '.dist-info', '.egg-info', '.pth']:
+                    continue
+                if item.is_dir():
+                    init_file = item / "__init__.py"
+                    if init_file.exists():
+                        packages.append(item.name)
+                    elif item.suffix == '.py':
+                        packages.append(item.stem)
+
+    return packages
+
+
+def build_with_site_packages():
+    print("Scan site-packages Path...")
+    print(f"Exclude List {excludeList}")
+    packages: 'list[str]' = get_all_site_packages()
+    print(f"Found {len(packages)} :")
+    cmd = [
+        sys.executable,
+        "-m",
+        "nuitka",
+    ]
+    cmd.extend([
+        "--standalone",
+        "--onefile",
+        "--output-dir=./dist",
+        "--output-filename=OlivOS",
+        "--windows-icon-from-ico=./resource/favoricon.ico",
+        "--windows-console-mode=attach",
+        "--follow-imports",
+        "--enable-plugin=tk-inter",
+        "--enable-plugin=pywebview",
+        "--low-memory",
+        "--jobs=2",
+        "--show-scons",
+        "--show-progress"
+    ])
+    for pkg in packages:
+        if pkg.lower() not in excludeList:
+            print(f"Hit {pkg}")
+            cmd.extend([f"--include-package={pkg}"])
+        else:
+            print(f"Skip {pkg}")
+    cmd.extend([
+        "main.py"
+    ])
+    print("\nbuild...")
+    subprocess.run(cmd)
+
+
+if __name__ == "__main__":
+    build_with_site_packages()
